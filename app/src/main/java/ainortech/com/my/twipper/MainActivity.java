3ab06ca9984a16fe44ff277c21b7532db8c91f1d
Ainor Syahrizal <macintorch@gmail.com>
2017-12-07 10:28:49 +0800

    initial commit

---
+++ app/src/main/java/ainortech/com/my/twipper/MainActivity.java
@@ -0,0 +1,13 @@
+package ainortech.com.my.twipper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
