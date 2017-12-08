8cfa536a064c511e826728ee5d49a8317aed8f46
Ainor Syahrizal <macintorch@gmail.com>
2017-12-08 11:31:19 +0800

    add test object to parse

--- app/src/main/java/ainortech/com/my/twipper/StarterApplication.java
+++ app/src/main/java/ainortech/com/my/twipper/StarterApplication.java
@@ -1,9 +1,13 @@
 package ainortech.com.my.twipper;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 /**
  * Created by ainorsyahrizal on 07/12/2017.
@@ -25,6 +29,21 @@ public class StarterApplication extends Application {
                 .server(getString(R.string.SERVER_URL))
                 .build()
         );
+
+        ParseObject object = new ParseObject("ExampleObject");
+        object.put("myNumber", "123");
+        object.put("myString", "putra");
+
+        object.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException ex) {
+                if (ex == null) {
+                    Log.i("Parse Result", "Successful!");
+                } else {
+                    Log.i("Parse Result", "Failed" + ex.toString());
+                }
+            }
+        });
 
         // ParseUser.enableAutomaticUser();
 
